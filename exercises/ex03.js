89ce5e9b870d7b60013b6f91708242d5c34bab6b
Ryoga <isimaruru.1712@gmail.com>
2025-02-18 20:50:03 -0800

    done

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -2,10 +2,36 @@
 // Remember the LIFO (Last-In, First-Out) rule
 // Make sure to build a temporary stack
 
-const Stack = require('../lib/Stack')
+const Stack = require("../lib/Stack");
 
 function removeDuplicates(stack) {
-  // your code here
+  let tempStack = new Stack();
+
+  while (!stack.isEmpty()) {
+    let removeElement = stack.pop();
+    let found = false;
+    let tempStorage = new Stack();
+
+    while (!tempStack.isEmpty()) {
+      let tempElement = tempStack.pop();
+      if (tempElement === removeElement) {
+        found = true;
+      }
+      tempStorage.push(tempElement);
+    }
+
+    while (!tempStorage.isEmpty()) {
+      tempStack.push(tempStorage.pop());
+    }
+
+    if (!found) {
+      tempStack.push(removeElement);
+    }
+  }
+
+  while (!tempStack.isEmpty()) {
+    stack.push(tempStack.pop());
+  }
 }
 
 // Create stack
@@ -17,5 +43,5 @@ stack.push(5);
 stack.push(1);
 stack.push(3);
 
-removeDuplicates(stack)
-console.log(stack.printStack()) // [5, 2, 1, 3]
+removeDuplicates(stack);
+console.log(stack.printStack()); // [5, 2, 1, 3]
